668947d54b12e6e1783d58c3e75a2d08c31efd96
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-06-07 22:48:31 +0800

    server.js ,htmlRoutes.js& apiRoutes added

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+//express configuration - tells node that we are creating an express server
+const app = express();
+
+//sets initial port
+const port = process.env.PORT || 3001;
+//middleware for parsing JSON and urlencoded from incoming data 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+//use routes
+app.use('./routes/apiRoutes');
+app.use('./routes/htmlRoutes');
+
+//starts the server to begin listenig
+app.listen(port, () => {
+    console.log(`API server now on port: ${port}!`);
+});
